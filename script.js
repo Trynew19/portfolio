c2d2b41cee5a2dcacdbab70c6b9db190676e0b6d
Trynew19 <chauhankrishana42@gmail.com>
2023-11-15 13:16:12 +0530

    sectoin complete

--- script.js
+++ script.js
@@ -7,3 +7,38 @@ function hideSidebar(){
   const sidebar = document.querySelector('.sidebar')
   sidebar.style.display = 'none'
 }
+
+
+var aboutebtn = document.querySelector('#aboute');
+var contactbtn = document.querySelector('#contact');
+var socialbtn = document.querySelector('#social');
+
+var aboutesec = document.querySelector('.aboute-section');
+var contactsec = document.querySelector('.contact-section');
+var socialsec = document.querySelector('.social-section');
+
+aboutebtn.addEventListener("click",function(){
+  aboutesec.style.display = "initial"
+})
+contactbtn.addEventListener("click",function(){
+  contactsec.style.display = "initial"
+})
+socialbtn.addEventListener("click",function(){
+  socialsec.style.display = "initial"
+})
+
+//mobile ke liye
+var aboutemob = document.querySelector('.aboutemob');
+var contactmob = document.querySelector('.contactmob');
+var socialemob = document.querySelector('.socialmob');
+
+
+aboutemob.addEventListener("click",function(){
+  aboutesec.style.display = "initial"
+})
+contactmob.addEventListener("click",function(){
+  contactsec.style.display = "initial"
+})
+socialemob.addEventListener("click",function(){
+  socialsec.style.display = "initial"
+})
